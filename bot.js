f64aa9f8580d6184c34da085d319b28586502b20
LadyLu <41097277+LadyLu@users.noreply.github.com>
2018-07-10 23:39:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'challenge') {
+    	message.reply('YOU DARE CHALLENGE THE ALMIGHTY MOTHSTAR?!');
   	}
 });
 
