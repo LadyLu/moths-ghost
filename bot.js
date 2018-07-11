97ff7bf1e9fb7868a69d83280c3383ab9381412b
LadyLu <41097277+LadyLu@users.noreply.github.com>
2018-07-11 00:16:51 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,31 @@ client.on('message', message => {
 
   if (message.content.includes("hellkits")) {
     message.channel.send("Ah, the devilish spawn of Bracken and Bear. Not even I will look down upon such kits.");
-  } 
+  } else
+
+  if (message.content.includes("brackenfall")) {
+    message.channel.send("Where is that deputy of mine?");
+  } else
+
+  if (message.content.includes("Brackenfall")) {
+    message.channel.send("Where is that deputy of mine?");
+  } else
+
+  if (message.content.includes("bracken")) {
+    message.channel.send("Where is that deputy of mine?");
+  } else
+
+  if (message.content.includes("Bracken")) {
+    message.channel.send("Where is that deputy of mine?");
+  }  else
+
+  if (message.content.includes("brackenstar")) {
+    message.channel.send("He is now a leader, eh? Better not turn my clan into carrion.");
+  } else
+
+  if (message.content.includes("Brackenstar")) {
+    message.channel.send("He is now a leader, eh? Better not turn my clan into carrion.");
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
