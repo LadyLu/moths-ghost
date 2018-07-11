9a11461849d9336132fd847cb8662da9fcd1d32d
LadyLu <41097277+LadyLu@users.noreply.github.com>
2018-07-10 23:59:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,12 +8,7 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'challenge') {
     	message.reply('YOU DARE CHALLENGE THE ALMIGHTY MOTHSTAR?!');
-  	} else
-
-def on_message(self, message):
-  if "Bracken" in message: {
-            self.send("That deputy would be nothing without my almighty glory!");
-  }
+  	} 
 });
 
 // THIS  MUST  BE  THIS  WAY
