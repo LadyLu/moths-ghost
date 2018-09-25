ef474941abfc545b6e70dc6726990cf33f97dd4e
LadyLu <41097277+LadyLu@users.noreply.github.com>
2018-09-25 18:20:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,10 +14,6 @@ client.on('message', message => {
     message.channel.send("Ah, the devilish spawn of Bracken and Bear. Not even I will look down upon such kits.");
   } else
 
-  if (message.content.includes("brackenfall")) {
-    message.channel.send("Where is that deputy of mine?");
-  } else
-      
   if (message.content === 'm_love') {
       message.reply('Ah... Yes.. *pat pat* You are a... Noble warrior? Yes, noble.');
   } else
